d68c0a9e10bb3fbec5d75b3b91b6f5f354f3b80d
naixsu <plnacua02@gmail.com>
2024-09-18 23:49:13 +0800

    Add movement when walk_f and walk_b are playing

--- pet.py
+++ pet.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk, ImageSequence
 
 # Define custom vars here
-MS = 50  # Delay for GIF frames in milliseconds
+# MS = 50  # Delay for GIF frames in milliseconds
 GIFS = [
     "resources/idle.gif",
     "resources/walk_f.gif",
@@ -18,9 +18,14 @@ class Pet:
         self.x_start = None
         self.y_start = None
         self.gif_label = None
+        self.ms = 50
         
         self.window = window
         self.load_all_gifs()  # Load all GIFs and their frames during initialization
+        
+        # Movement control
+        self.move_direction = None
+        self.move_speed = 5  # Speed of movement in pixels
     
     def load_all_gifs(self):
         for gif_path in GIFS:
@@ -55,10 +60,25 @@ class Pet:
         if ind == len(self.gif):
             ind = 0
             # Schedule switch only after the last frame is displayed
-            self.window.after(MS, self.switch_gif)  # Use MS delay to ensure the last frame is shown
+            self.window.after(self.ms, self.switch_gif)  # Use self.ms delay to ensure the last frame is shown
             return
-        # MS = ms, basically adjust the speed of the gif
-        self.window.after(MS, self.update_gif, ind)
+        
+        # Move window based on current GIF
+        if self.move_direction:
+            self.move_window()
+        
+        # self.ms = self.ms, basically adjust the speed of the gif
+        self.window.after(self.ms, self.update_gif, ind)
+
+    def move_window(self):
+        x, y = self.window.winfo_x(), self.window.winfo_y()
+        if self.move_direction == 'left':
+            x -= self.move_speed
+        elif self.move_direction == 'right':
+            x += self.move_speed
+        
+        # Update window position
+        self.window.geometry(f"+{x}+{y}")
     
     def switch_gif(self):
         # Clear the label's image to avoid flickering
@@ -68,6 +88,21 @@ class Pet:
         self.current_gif = (self.current_gif + 1) % len(GIFS)
         self.gif_path = GIFS[self.current_gif]
         self.gif = self.frames[self.gif_path]
+        
+        # Update movement direction based on GIF
+        if "walk_f" in self.gif_path:
+            self.move_direction = 'left'
+        elif "walk_b" in self.gif_path:
+            self.move_direction = 'right'
+        else:
+            self.move_direction = None
+        
+        # This is just to smooth out the animation frames
+        # NOTE: The `ms` numbers are all arbitrary
+        if self.move_direction is None: # Idling
+            self.ms = 50
+        else:
+            self.ms = 70
         
         print(f"Switching to GIF {self.current_gif} with {len(self.gif)} frames")
 
